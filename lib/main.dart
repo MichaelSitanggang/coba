4fc5da4a5fe85b424edf59f0f520f69fe7be504b
231130399 <grace.michelle@students.mikroskil.ac.id>
2024-05-27 16:24:32 +0700

    pertama

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: myHomePage(),
+    );
+  }
+}
+
+class myHomePage extends StatefulWidget {
+  const myHomePage({super.key});
+
+  @override
+  State<myHomePage> createState() => _myHomePageState();
+}
+
+class _myHomePageState extends State<myHomePage> {
+  int index = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget scrren() {
+      if (index == 0) {
+        return Scrren1();
+      } else {
+        return Scrren2();
+      }
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Provider"),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.delete),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          notchMargin: 6,
+          shape: CircularNotchedRectangle(),
+          color: Colors.lightBlue,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            onTap: (value) {
+              setState(() {
+                index = value;
+              });
+            },
+            currentIndex: index,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings), label: "Settings")
+            ],
+          ),
+        ),
+        body: scrren());
+  }
+}
+
+class Scrren1 extends StatelessWidget {
+  const Scrren1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Ini scrren 1");
+  }
+}
+
+class Scrren2 extends StatelessWidget {
+  const Scrren2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Ini scrren 2");
+  }
+}
